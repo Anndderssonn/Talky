2ff7855ae8063ae102dad58b869d9f95e0e10c36
Anderson Valencia <andersson@Anderssons-MacBook-Pro.local>
2025-05-08 22:53:05 -0500

    First commit

---
+++ Talky/TalkyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TalkyApp.swift
+//  Talky
+//
+//  Created by Andersson on 8/05/25.
+//
+
+import SwiftUI
+
+@main
+struct TalkyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
